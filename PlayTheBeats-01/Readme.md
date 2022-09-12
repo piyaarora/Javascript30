a52d5ff0a8ee6058307c8044af2e8e20b9efbe41
Priya Arora <39625430+piyaarora@users.noreply.github.com>
2022-09-12 09:37:50 +0530

    Initial commit

---
+++ PlayTheBeats-01/Readme.md
@@ -0,0 +1,1 @@
+1. To play the sound, click the corresponding key on the keyboard.
---
+++ PlayTheBeats-01/script.js
@@ -0,0 +1,27 @@
+
+
+const playBeat = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.addEventListener('click', function () {
+        console.log(key)
+    })
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing')
+}
+
+const removeTransition = (e) => {
+    if (e.propertyName !== 'transform') return;
+    // this is replaced with e.currentTarget for es6 syntax
+    e.currentTarget.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playBeat);
+
+
+
